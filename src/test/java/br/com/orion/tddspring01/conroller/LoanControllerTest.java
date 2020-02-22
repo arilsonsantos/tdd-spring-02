eb6c19ac6afc32151f2a16b9ec05868cc14c5cad
Arilson Santos <arilson@msn.com>
2020-02-22 20:13:45 -0300

    LoanControllerTest

--- src/test/java/br/com/orion/tddspring01/conroller/LoanControllerTest.java
+++ src/test/java/br/com/orion/tddspring01/conroller/LoanControllerTest.java
@@ -2,6 +2,7 @@ package br.com.orion.tddspring01.conroller;
 
 
 import br.com.orion.tddspring01.controller.LoanController;
+import br.com.orion.tddspring01.exceptions.BookAlreadyLoanedException;
 import br.com.orion.tddspring01.exceptions.ResourceNotFoundException;
 import br.com.orion.tddspring01.model.Book;
 import br.com.orion.tddspring01.model.Loan;
@@ -77,6 +78,27 @@ public class LoanControllerTest extends AbstractControllerTest {
 
         mvc.perform(request).andExpect(status().isNotFound())
         .andExpect(jsonPath("message").value("Book not found by Isbn :" + dto.getIsbn()));
+
+    }
+
+    @Test
+    @DisplayName("Must return an error")
+    public void loanedBookErrorOnCreateBookLoan() throws Exception {
+        LoanDto dto = LoanDto.builder().isbn("123").customer("Jose").build();
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        Book book = Book.builder().id(1L).title("Book A").author("John Silva").isbn("123").build();
+        BDDMockito.given(bookService.getBookByIsbn("123")).willReturn(Optional.of(book));
+
+        BDDMockito.given(loanService.save(Mockito.any(Loan.class))).willThrow(new BookAlreadyLoanedException("Book already loaned"));
+
+        final MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(LOAN_API)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message").value("Book already loaned"));
 
     }
 
